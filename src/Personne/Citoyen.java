7f82af777c9495aec0ed6c4ab8e159f3b53d382e
viper23 <htonnycarlos@gmail.com>
2022-07-25 22:26:54 +0200

    method classe

--- src/Personne/Citoyen.java
+++ src/Personne/Citoyen.java
@@ -1,5 +1,7 @@
 package Personne;
 
+import InputOutput.Io;
+
 import java.util.ArrayList;
 
 public class Citoyen {
@@ -66,6 +68,54 @@ public class Citoyen {
 
     public void setLieu_de_naissance(String lieu_de_naissance) {
         this.lieu_de_naissance = lieu_de_naissance;
+    }
+
+    // les methodes de la classe
+
+    public static void EnregistrerCitoyen(){
+        new Citoyen(InputOutput.Io.setString("\nNom : "),InputOutput.Io.setString("\nPrenom : "),
+                Io.setString("Sexe :"),Io.setINT("Année de naissance :"),Io.setString("Lieu de naissance :"));
+        System.out.print("\n******* Le Citoyen a été enregistré avec success *******\n");
+    }
+
+    public static void AfficherCitoyen(){
+        if (!liste_de_tous_les_citoyens.isEmpty()){
+            System.err.print("\n******* LISTE DE TOUS LES CITOYENS *******\n");
+            for (int i = 0; i < liste_de_tous_les_citoyens.size(); i++) {
+                System.out.print(
+                        i+1+"  Nom :"+liste_de_tous_les_citoyens.get(i).getNom()+"\t"+
+                                "Prenom :"+liste_de_tous_les_citoyens.get(i).getPrenom()+"\t"+
+                                "Sexe :"+liste_de_tous_les_citoyens.get(i).getSexe()+"\t"+
+                                "Année Naissance :"+liste_de_tous_les_citoyens.get(i).getAnnee_de_naissance()+"\t"+
+                                "Lieu de naissance :"+liste_de_tous_les_citoyens.get(i).getLieu_de_naissance()+"\n"+"\n"
+                );
+            }
+        }else
+            System.err.print("\n******* Erreur Aucun Citoyen n'a été enregistré *******\n");
+    }
+
+    public static void modifierCitoyen(){
+        if (!liste_de_tous_les_citoyens.isEmpty()){
+            System.err.print("\n******* LISTE DE TOUS LES DONNEURS *******\n");
+            AfficherCitoyen();
+            liste_de_tous_les_citoyens.set(InputOutput.Io.setINT("Numero du citoyen à modifier :")-1,
+                    new Citoyen(InputOutput.Io.setString("\nNom : "),InputOutput.Io.setString("\nPrenom : "),
+                            Io.setString("Sexe :"),Io.setINT("Année de naissance :"),Io.setString("Lieu de naissance :")));
+            System.err.print("\n******* LISTE DE TOUS LES CITOYENS MISE A JOUR *******\n");
+            liste_de_tous_les_citoyens.remove(liste_de_tous_les_citoyens.size()-1);
+            AfficherCitoyen();
+        }else
+            System.err.print("\n******* Erreur Aucun Citoyen n'a été enregistré *******\n");
+
+    }
+
+    public static void supprimerCitoyen(){
+        if (!liste_de_tous_les_citoyens.isEmpty()){
+            AfficherCitoyen();
+            liste_de_tous_les_citoyens.remove(InputOutput.Io.setINT("Numero du citoyen à supprimer :")-1);
+            AfficherCitoyen();
+        }else
+            System.err.print("\n******* Erreur Aucun Citoyen n'a été enregistré *******\n");
     }
 
 
